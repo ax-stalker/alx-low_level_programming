5288513b74e5821049cdd961e73c9b6ff8e6a74f
ax-stalker <maxwellmwangi977@gmail.com>
2023-02-21 13:13:18 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,11 +3,11 @@
 
 /* more headers goes there */
 #include <stdio.h>
-/* betty style doc for function main goes there */
+
 /**
- * main= main function
+ * main - Entry point
  *
- * Return: always 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
