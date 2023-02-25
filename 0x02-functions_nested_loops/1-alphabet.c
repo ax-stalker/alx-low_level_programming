e4737d83e9b1fbc68e4d97372718441d67d833ed
ax-stalker <maxwellmwangi977@gmail.com>
2023-02-25 04:00:45 -0800

    0x02 Functions Nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,19 @@
-#include "holberton.h"
-/**
- * print_alphabet - prints the english alphabet from a-z.
- * Return: Nothing.
- */
-void print_alphabet(void)
-{
-	char c;
+#include "main.h"
 
-	for (c = 'a'; c <= 'z'; c++)
+
+	/**
+	 * print_alphabet - print all alphabet in lowercase
+	 */
+
+
+	void print_alphabet(void)
 	{
-		_putchar(c);
+		char letter;
+
+
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+
+
+		_putchar('\n');
 	}
-	_putchar('\n');
-}
