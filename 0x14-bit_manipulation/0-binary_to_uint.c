301ff6a391ec4eb3405eedca45fbd89bdfb09257
ax-stalker <maxwellmwangi977@gmail.com>
2023-07-25 05:13:12 -0700

    made changes to the result line

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -19,15 +19,15 @@ unsigned int binary_to_uint(const char *b)
     int string_length = strlen(b);
     int i;
 
-    for (i = 0; i < string_length; i++) {
+    for (i = 0; i < string_length; i++) 
+    {
         char character = b[i];
         if (character != '0' && character != '1')
             return 0;
 
-        response <<= 1;
-        response += character - '0';
-    }
+        response =2 * response + (b[i]-'0';
+			
+     }
 
     return response;
-}
-	
+}	
