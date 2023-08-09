5efaacf0a51e86659ab47039f6a19953b7f4c01a
ax-stalker <maxwellmwangi977@gmail.com>
2023-08-09 12:58:25 -0700

    haha

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include <fcntl.h> // Include for the open function
-#include <unistd.h> // Include for the write and close functions
 
 /**
  * create_file - Creates a file.
@@ -12,29 +10,24 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    int fd, w, len = 0;
+	int fd, w, len = 0;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    if (text_content != NULL)
-    {
-        for (len = 0; text_content[len]; len++)
-            ;
-    }
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
 
-    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-    if (fd == -1)
-        return (-1);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
 
-    w = write(fd, text_content, len);
-    if (w == -1)
-    {
-        close(fd);
-        return (-1);
-    }
+	if (fd == -1 || w == -1)
+		return (-1);
 
-    close(fd);
+	close(fd);
 
-    return (1);
+	return (1);
 }
